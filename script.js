9d991394a23ee2e34fa09559bdadec2316c015ad
imAnkit7 <116709086+imAnkit7@users.noreply.github.com>
2023-06-30 12:38:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let inputTask=document.getElementById('inputval')
+let list=document.querySelector('.list')
+window.addEventListener('keypress',(e)=>{
+    if(e.key==="Enter"){
+        handleClick()
+    }
+})
+const handleClick=()=>{
+    if(inputTask.value == ""){
+        alert('please enter task')
+    }
+    else{
+        // localStorage.setItem('todo',inputTask.value)
+        let newEle=document.createElement('div')
+        newEle.innerHTML=`${inputTask.value} <i class="fa-solid fa-trash"></i>`
+        list.appendChild(newEle)
+        inputTask.value=""
+        newEle.querySelector('i').addEventListener('click',()=>{
+            newEle.remove()
+        })
+    }
+}
